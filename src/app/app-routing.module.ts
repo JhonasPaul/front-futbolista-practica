3ed739b802b21ce6783afec42e9fdcd767fa441c
jonathan <johnkord17@gmail.com>
2024-11-14 02:46:41 -0500

    uno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPageComponent} from './shared/pages/home-page/main-page.component';
+import {BuscarFutbolistaComponent} from './futbolista/pages/buscar-futbolista/buscar-futbolista.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'futbolistas/page/:page',
+    component: MainPageComponent
+  },
+  {
+    path: 'futbolistas',
+    component: MainPageComponent
+  },
+  {
+    path: 'buscar',
+    component: BuscarFutbolistaComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'futbolistas',
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:
+    [
+      RouterModule
+    ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
